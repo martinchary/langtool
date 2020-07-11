2d0816c01286f1c8e65d2bec508ba92dc615fe6c
Martin <martochary@gmail.com>
2020-07-10 23:08:10 -0300

    can now add new terms through flask

--- page.py
+++ page.py
@@ -1,5 +1,5 @@
-from flask import Flask, redirect, url_for, render_template
-from __init__ import json_to_list
+from flask import Flask, redirect, url_for, render_template, request
+from langtool import json_to_list, add_term
 
 app = Flask(__name__)
 
@@ -13,6 +13,23 @@ def namepage(name):
     return render_template('index.html', content=name, terms=json_to_list())
 
 
+@app.route("/addTerm/", methods=['POST', 'GET'])
+def addTerm():
+    if request.method == "POST":
+        term = request.form['tm']
+        trans = request.form['tl']
+        add_term(term, trans)
+        # return redirect(url_for("user", usr=term+trans))
+        return redirect(url_for("termlist"))
+    else:
+        return render_template("addTerm.html")
+
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+
 @app.route("/list/")
 def termlist():
     return render_template("termlist.html", terms=json_to_list())
@@ -24,4 +41,4 @@ def admin():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
